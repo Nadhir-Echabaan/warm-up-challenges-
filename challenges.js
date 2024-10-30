4e975867500861ac75fca83a922cdb8060d8960e
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:48:43 +0100

    challenge 1 / wram up 2

--- challenges.js
+++ challenges.js
@@ -1,8 +1,6 @@
-//ex 14
-function factoriel (n) {
-  let fact = 1 ;
-  for (let i = n ; i > 1 ; i--) {
-      fact = fact * i
-  }
-  return fact
+//ex 1
+function removeStr(str,chr,i) {
+  if (str[i] === chr) console.log(str.replace(str[i],''))
+  else if (str.indexOf(chr) === -1) console.log(`${chr} does not exist in this string`)
+  else console.log(`${chr} does not exist at this position`)
 }
