6e70af9aae2e0174f3f5c32a69138d66058c106e
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 09:00:29 +0100

    challenge 9 / warm up 2

--- challenges.js
+++ challenges.js
@@ -1,9 +1,8 @@
-//ex 8
-for (let i = 1 ; i < 101 ; i++) {
-  let msg = ''
-  if (i % 5 === 0 && i % 3 === 0) msg = 'FizzBuzz';
-  else if (i % 3 === 0) msg = 'Fizz';
-  else if (i % 5 === 0) msg = 'Buzz'
-  else msg = `${i}`
-  console.log(msg)
-}
+//ex 9
+function stars_triangle() {
+  let symbole = ''
+  for (let i = 0 ; i < 5 ; i++) {
+      symbole = symbole + '*'
+      console.log(symbole)
+  }
+};
