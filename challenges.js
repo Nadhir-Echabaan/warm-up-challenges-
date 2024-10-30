9220c61e0a27b87d64746ee9e52c56489612069e
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:58:55 +0100

    challenge 8 / warm up 2

--- challenges.js
+++ challenges.js
@@ -1,4 +1,9 @@
-//ex 7
-arr.sort(function(a,b){
-  return a-b
-})
+//ex 8
+for (let i = 1 ; i < 101 ; i++) {
+  let msg = ''
+  if (i % 5 === 0 && i % 3 === 0) msg = 'FizzBuzz';
+  else if (i % 3 === 0) msg = 'Fizz';
+  else if (i % 5 === 0) msg = 'Buzz'
+  else msg = `${i}`
+  console.log(msg)
+}
