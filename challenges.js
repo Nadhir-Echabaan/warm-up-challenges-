289e375dedaa1f8101b1ab843dec7dde6a17cf48
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:56:16 +0100

    challenge 6 / warm up 2

--- challenges.js
+++ challenges.js
@@ -1,8 +1,4 @@
-//ex 5
-function swapHalves(arr) {
-  const mid = arr.length / 2;
-  const firstHalf = arr.slice(0, mid);
-  const secondHalf = arr.slice(mid);
- 
-  return secondHalf.concat(firstHalf);
+//ex 6
+function JOIN(arr) {
+  return arr.join('+')
 }
