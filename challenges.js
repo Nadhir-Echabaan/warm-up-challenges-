a3c51a9f572b32744342878f9446936c09319203
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:33:06 +0100

    challenge 4 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,14 +1,13 @@
-// ex 3
-function filter_array_values(arr) {
-  let falsy = [false, 0, "", "", ``, null];
-  let i = 0;
-  let newArr = [];
-  while (i < arr.length) {
-    if (falsy.indexOf(arr[i]) !== -1) i++;
-    else {
-      newArr.push(arr[i]);
-      i++;
-    }
-  }
-  return newArr;
+// ex 4
+function date_diff_indays(date1, date2) {
+  // Date 1
+  let day1 = date1.slice(0, 2);
+  let month1 = date1.slice(3, 5);
+  let year1 = date1.slice(6);
+  // Date 2
+  let day2 = date2.slice(0, 2);
+  let month2 = date2.slice(3, 5);
+  let year2 = date2.slice(6);
+  //difference
+  return (year2 - year1) * 356 + (month2 - month1) * 30 + (day2 - day1);
 }
