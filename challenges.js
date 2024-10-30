57b73844c52484a32ca1a78265bb35834d5ff5e7
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:31:46 +0100

    challenge 3 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,9 +1,14 @@
-// ex 2
-function flattenArray(arr) {
-  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
-}
-function difference(arr1, arr2) {
-  const combined = flattenArray(arr1).concat(flattenArray(arr2));
-  const uniqueElements = [...new Set(combined)];
-  return uniqueElements.sort((a, b) => a - b);
+// ex 3
+function filter_array_values(arr) {
+  let falsy = [false, 0, "", "", ``, null];
+  let i = 0;
+  let newArr = [];
+  while (i < arr.length) {
+    if (falsy.indexOf(arr[i]) !== -1) i++;
+    else {
+      newArr.push(arr[i]);
+      i++;
+    }
+  }
+  return newArr;
 }
