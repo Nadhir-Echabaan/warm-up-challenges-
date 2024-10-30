6d315d251a8011f880c5b53c25363f442f466553
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:38:21 +0100

    challenge 10 / wram up 1

--- challenges.js
+++ challenges.js
@@ -1,9 +1,14 @@
-//ex 9
-function humanize_format (num) {
-  num = num +''
-  if (num[num.length - 1] === '1') num = num + 'st'
-  else if (num[num.length - 1] === '2') num = num + 'nd'
-  else if (num[num.length - 1] === '3') num = num + 'rd'
-  else num = num + 'th'
-  return num
+//ex 10
+function FlatCase(str) {
+  let test = true;
+  let i = 0 ;
+  if (typeof str !== 'string') {
+      test = false
+      console.log('it must be a string')
+  }
+  while (test && i < str.length) {
+      if (str[i] === str[i].toUpperCase()) test = false
+      else i++
+  }
+  return test
 }
