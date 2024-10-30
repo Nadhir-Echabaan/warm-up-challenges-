44b20151e48015c58e569fa7a3eb090313a6c3db
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:35:34 +0100

    challenge 8 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,13 +1,9 @@
-// ex 4
-function date_diff_indays(date1, date2) {
-  // Date 1
-  let day1 = date1.slice(0, 2);
-  let month1 = date1.slice(3, 5);
-  let year1 = date1.slice(6);
-  // Date 2
-  let day2 = date2.slice(0, 2);
-  let month2 = date2.slice(3, 5);
-  let year2 = date2.slice(6);
-  //difference
-  return (year2 - year1) * 356 + (month2 - month1) * 30 + (day2 - day1);
+//ex 8
+function swapcase(str) {
+  let new_str = ''
+  for (let i = 0 ; i < str.length ; i++) {
+      if (str[i] === str[i].toUpperCase()) new_str = new_str + str[i].toLowerCase()
+      else if (str[i] === str[i].toLowerCase()) new_str = new_str + str[i].toUpperCase()
+  }
+  return new_str
 }
