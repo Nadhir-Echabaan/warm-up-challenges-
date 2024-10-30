892c643c1db760b27de59b68a6a89a422177ddee
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:55:06 +0100

    challenge_5 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,4 +1,8 @@
-//ex 4
-function replaceFirstDigit(str) {
-  return str.replace(/\d/, '$');
+//ex 5
+function swapHalves(arr) {
+  const mid = arr.length / 2;
+  const firstHalf = arr.slice(0, mid);
+  const secondHalf = arr.slice(mid);
+ 
+  return secondHalf.concat(firstHalf);
 }
