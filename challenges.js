398194023689d9089dd20841a3f79906bf28ec06
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 09:40:25 +0100

    challenge 1 / warm up 2

--- challenges.js
+++ challenges.js
@@ -1,16 +1,6 @@
-"use strict";
-// warm up 1 : ex 1
-function unzip(arr) {
-  let output = [];
-  let NumArr = [];
-  let StrArr = [];
-  let BoolArr = [];
-  arr = arr.flat();
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === "number") NumArr.push(arr[i]);
-    else if (typeof arr[i] === "boolean") BoolArr.push(arr[i]);
-    else if (typeof arr[i] === "string") StrArr.push(arr[i]);
-  }
-  output.push(NumArr, StrArr, BoolArr);
-  return output;
+//ex 1
+function removeStr(str,chr,i) {
+  if (str[i] === chr) console.log(str.replace(str[i],''))
+  else if (str.indexOf(chr) === -1) console.log(`${chr} does not exist in this string`)
+  else console.log(`${chr} does not exist at this position`)
 }
