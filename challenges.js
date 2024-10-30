9d5edead179410b4fe53301af4c9acf033ecfc9c
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:39:21 +0100

    challenge 11 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,14 +1,4 @@
-//ex 10
-function FlatCase(str) {
-  let test = true;
-  let i = 0 ;
-  if (typeof str !== 'string') {
-      test = false
-      console.log('it must be a string')
-  }
-  while (test && i < str.length) {
-      if (str[i] === str[i].toUpperCase()) test = false
-      else i++
-  }
-  return test
+//ex 11
+function isPropExist(object , property) {
+  return object.hasOwnProperty(property)
 }
