ece86b57249ce97e8e256af8883148d2fbb7c821
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:54:05 +0100

    challenge 4 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,19 +1,4 @@
-//ex 3
-function Biggest_Even_Number (arr) {
-  let test = false
-  let i = 0
-  let max = 'there is no even number';
-  while (i < arr.length && !test) {
-      if (arr[i] % 2 === 1) {
-          max = arr[i]
-          test = true
-      }
-      else i++
-  }
-  i++
-  while(i<arr.length) {
-      if (arr[i] % 2 === 1 && arr[i] > max) max = arr[i]
-      i++
-  }
-  return max
+//ex 4
+function replaceFirstDigit(str) {
+  return str.replace(/\d/, '$');
 }
