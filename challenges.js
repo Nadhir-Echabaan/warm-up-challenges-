01b582dd6010f3fed2ab0a59e00db1b17964e033
Nadhir-Echabaan <nadhirchabane17@gmail.com>
2024-10-30 08:52:15 +0100

    challenge 3 / warm up 1

--- challenges.js
+++ challenges.js
@@ -1,9 +1,18 @@
-//ex 2
-function longestStr (arr) {
-  let i = 1
-  let max = arr[0]
-  while (i < arr.length) {
-      if (typeof arr[i] === 'string' && arr[i].length  > max.length) max = arr[i]
+//ex 3
+function Biggest_Even_Number (arr) {
+  let test = false
+  let i = 0
+  let max = 'there is no even number';
+  while (i < arr.length && !test) {
+      if (arr[i] % 2 === 1) {
+          max = arr[i]
+          test = true
+      }
+      else i++
+  }
+  i++
+  while(i<arr.length) {
+      if (arr[i] % 2 === 1 && arr[i] > max) max = arr[i]
       i++
   }
   return max
